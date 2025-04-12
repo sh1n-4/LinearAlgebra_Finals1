2f5fed3dc8f002745d6a31c9177db25322ce30de
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-04-12 14:02:03 +0800

    Add reflection

--- Assets/Scripts/RectangleGenerator.cs
+++ Assets/Scripts/RectangleGenerator.cs
@@ -95,15 +95,15 @@ public Material rectangleMaterial;
 
     private Vector3[] GetRectangleFace(float zOffset, Quaternion rotation, Vector3 center, Vector3 size)
     {
-        float hw = size.x * 0.5f;
-        float hh = size.y * 0.5f;
+        float hw = size.x;
+        float hh = size.y;
 
         Vector3[] corners = new Vector3[]
         {
-            new Vector3(-hw, -hh, zOffset),
-            new Vector3(hw, -hh, zOffset),
+            new Vector3(0, 0, zOffset),
+            new Vector3(hw, 0, zOffset),
             new Vector3(hw, hh, zOffset),
-            new Vector3(-hw, hh, zOffset)
+            new Vector3(0, hh, zOffset)
         };
 
         for (int i = 0; i < corners.Length; i++)
